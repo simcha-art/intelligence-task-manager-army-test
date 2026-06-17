1996d6734fbfd700c2c38f42ece4929c538f4df5
simcha <simchacop@gmail.com>
2026-06-17 15:47:15 +0300

    feat: add AgentDB with all its methods

--- database/agent_db.py
+++ database/agent_db.py
@@ -1,31 +1,131 @@
-"""
-AgentDB
 
-|method                         | goal                                             | returns                                                           |
-|-------------------------------|--------------------------------------------------|-------------------------------------------------------------------|
-|create_agent(data)             |adds a new agent                                  |dictionary of the new agent                                        |
-|get_all_agents()               |returns a list of all existing agents             |list of all agents                                                 |
-|get_agent_by_id(agent_id)      |returns the agent with id == id                   |dictionary of the agent or None if not exist                       |
-|update_agent(agent_id, data)   |updating details of existing agent with id == id  |True(success) or False (failed)                                    |
-|deactivate_agent(agent_id)     |updating feild "is_active" to False               |True(success) or False (failed)                                    |
-|increment_completed(agent_id)  |adding 1 to field "comleted_missions"             |True(success) or False (failed)                                    |
-|increment_completed(agent_id)  |adding 1 to field "comleted_missions"             |True(success) or False (failed)                                    |
-|increment_failed(agent_id)     |adding 1 to field "failed_missions"               |True(success) or False (failed)                                    |
-|get_agent_performance(agent_id)|returns a summary of the performance of the agent |dict {total: int, completed: int, failed: int, success_rate: completed / total * 100}
-|count_active_agents()          |counts the number of active agents                |int                                                                 |
-"""
-
-from database.db_connection import DB_conn
+from db_connection import DB_conn
 import mysql.connector
 
-            # name VARCHAR(100) NOT NULL,
-            # specialty VARCHAR(150) NOT NULL,
-            # is_active BOOLEAN DEFAULT TRUE,
-            # completed_missions INT DEFAULT 0,
-            # failed_missions INT DEFAULT 0,
-            # agent_rank ENUM('Junior', 'Senior', 'Commander')
-            # )
 
 class AgentDB:
     def create_agent(self, data: dict):
         name = data.get("name")
+        specialty = data.get('specialty')
+        agent_rank = data.get("agent_rank")
+        query = """
+        INSERT INTO agents
+        (name, specialty, agent_rank)
+        VALUES
+        (%s, %s, %s)
+        """
+        with DB_conn.conn.cursor(dictionary=True) as cursor:
+            cursor.execute(query, (name, specialty, agent_rank))
+            DB_conn.conn.commit()
+            success = cursor.rowcount > 0
+            if not success:
+                return None
+            new_id = cursor.lastrowid
+            cursor.execute(f"SELECT * FROM agents WHERE id = {new_id}")
+            return cursor.fetchone()
+
+
+    def get_all_agents(self):
+        with DB_conn.conn.cursor(dictionary=True) as cursor:
+            cursor.execute("SELECT * FROM agents")
+            return cursor.fetchall()
+        
+    def get_agent_by_id(self, agent_id):
+        with DB_conn.conn.cursor(dictionary=True) as cursor:
+            cursor.execute("SELECT * FROM agents WHERE id = %s", (agent_id,))
+            return cursor.fetchone()
+        
+    def update_agent(self, agent_id: int, data: dict):
+        if "id" in data: 
+            raise ValueError ("must not update id")
+        set_cluase = ", ".join(f"{feild} = %s " for feild in data.keys())
+        values = list(data.values())
+        params = values + [agent_id]
+
+        query = f"""
+        UPDATE agents
+        SET
+        {set_cluase}
+        WHERE id = %s
+        """
+
+        with DB_conn.conn.cursor(dictionary=True) as cursor:
+            cursor.execute(query, params)
+            DB_conn.conn.commit()
+            same = cursor.warning_count == 0
+            success = cursor.rowcount > 0
+            if same:
+                return "agent already up to date"
+            if success:
+                return "updated successfully"
+            else:
+                return "update failed"
+        
+    def deactivate_agent(self, agent_id):
+        query = "UPDATE agents SET is_active = FALSE WHERE ID = %s"
+        with DB_conn.conn.cursor(dictionary=True) as cursor:
+            cursor.execute(query, (agent_id,))
+            DB_conn.conn.commit()
+            success = cursor.rowcount > 0   
+            if success:
+                return "deactivated successfully"
+            else:
+                return "deactivate failed" 
+        
+    def increment_completed(self, agent_id):
+        with DB_conn.conn.cursor() as cursor:
+            cursor.execute("UPDATE agents SET completed_missions = completed_missions + 1 WHERE id = %s", (agent_id,))
+            DB_conn.conn.commit()
+            success = cursor.rowcount > 0
+            if success:
+                return "feild completed_missions incremented successfully"
+            else:
+                return "failed to increment"
+    
+    def increment_failed(self, agent_id):
+        with DB_conn.conn.cursor() as cursor:
+            cursor.execute("UPDATE agents SET failed_missions = failed_missions + 1 WHERE id = %s", (agent_id,))
+            DB_conn.conn.commit()
+            success = cursor.rowcount > 0
+            if success:
+                return "feild failed_missions incremented successfully"
+            else:
+                return "failed to increment"
+            
+    def get_agent_performance(self, agent_id):
+        query = "SELECT completed_missions, failed_missions FROM agents WHERE id = %s"
+        with DB_conn.conn.cursor(dictionary=True) as cursor:
+            cursor.execute(query, (agent_id,))
+            result = cursor.fetchone()
+
+        if not result:
+            return f"agent {agent_id} not found"
+        
+        completed_missions = result["completed_missions"]
+        failed_missions = result["failed_missions"]
+        total = completed_missions + failed_missions
+        success_rate = None if total == 0 else completed_missions / total * 100
+
+        return {
+            "total": total,
+            "completed_missions": completed_missions,
+            "failed_missions": failed_missions,
+            "success_rate": success_rate
+                }
+    
+    def count_active_agents(self):
+        query = """
+        SELECT COUNT(*) AS active_agents FROM agents
+        WHERE is_active = TRUE
+        """
+        with DB_conn.conn.cursor(dictionary=True) as cursor:
+            cursor.execute(query)
+            return cursor.fetchone()
+
+
+
+
+
+
+
+agent_manager = AgentDB()
