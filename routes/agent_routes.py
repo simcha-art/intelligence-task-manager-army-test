a303de592a8726d1c2e560ff90c8375369ca88ba
simcha <simchacop@gmail.com>
2026-06-18 12:44:38 +0300

    feat: add more validation so it will handle mysql errors, maybe there are more, in the end should delete the last 'raise' written

--- routes/agent_routes.py
+++ routes/agent_routes.py
@@ -11,6 +11,7 @@ from pydantic import BaseModel
 from fastapi import APIRouter, HTTPException
 from logs.logger_config import logger
 from database.agent_db import agent_manager
+from mysql.connector import Error as MYSQL_ERROR
 
 router = APIRouter()
 
@@ -24,15 +25,25 @@ def handle_id(agent_id: str)->int:
 
 @router.post("", status_code=201)
 def add_new_agent(data: dict):
-    # validation happends inside the mysql server and caught by exeptions_handler in main.py
-    logger.info("post/agents called")
-    logger.info("adding a new agent")
-    new_agent = agent_manager.create_agent(data)
-    logger.info("agent created successfully")
-    return new_agent
+    try:
+        if len(data) > 3:
+            logger.error(f"user entered {list(data.keys())}, but only [name, specialty, agent rank] are valid")
+            raise HTTPException(422, "only feilds [name, specialty, agent rank] are valid")
+        logger.info("post/agents called")
+        logger.info("adding a new agent")
+        new_agent = agent_manager.create_agent(data)
+        logger.info("agent created successfully")
+        return new_agent
+    except MYSQL_ERROR as e:
+        logger.error(f"code: {e.errno}, msg: {e.msg}")
+        if e.errno == 1048:
+            raise HTTPException(422, "feilds [name, specialty, agent rank] must be filled")
+        elif e.errno == 1265:
+            raise HTTPException(422, "feild agent_rank must be filled with 'Junior', 'Senior' or 'Commander'")
+        raise
 
 
-@router.get("")
+@router.get("") 
 def get_all_agents():
     logger.info("get/agents called")
     logger.info("getting list of all agents...")
@@ -69,10 +80,21 @@ def update_agent(agent_id, data: dict):
     if not data:
         logger.error("user didn't enter any data")
         raise HTTPException(400, "you must enter some data")
+    
+    elif len(data) > 4:
+        raise HTTPException(422, "only feilds [name, specialty, agent rank] can be filled")
+
 
 
     logger.info(f"start updating agent {agent_id}...")
-    message = agent_manager.update_agent(agent_id, data)
+    try:
+        message = agent_manager.update_agent(agent_id, data)
+    except MYSQL_ERROR as e:
+        logger.error(f"code: {e.errno}, msg: {e.msg}")
+        if e.errno == 1265:
+            raise HTTPException(422, "feild agent_rank must be filled with 'Junior', 'Senior' or 'Commander'")
+        raise
+
     logger.info(f"agent {agent_id} updated successfully")
     return message
 
